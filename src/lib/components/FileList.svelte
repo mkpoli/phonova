6e2e87659136d09e7e795cf8088a9191d3f04c9c
mkpoli <git@mkpo.li>
2025-01-07 07:31:43 +0900

    feat: add description and buttons to audio list

--- src/lib/components/FileList.svelte
+++ src/lib/components/FileList.svelte
@@ -2,12 +2,16 @@
   const files: { name: string }[] = [];
 </script>
 
-<section class="bg-gray-200 dark:bg-gray-800">
+<section class="bg-gray-200 dark:bg-gray-800 grid grid-rows-[auto_1fr] w-64">
   <h2 class="bg-gray-300 dark:bg-black p-2">Audios</h2>
 
   <div class="p-2 h-full">
     {#if files.length === 0}
-      <p>No files found</p>
+      <div class="flex flex-col items-center justify-start h-full gap-2">
+        <p>To start, add an audio file, you can drag and drop it here or</p>
+        <button class="button-primary">Open a file</button>
+        <button class="button-primary" disabled>Record audio</button>
+      </div>
     {:else}
       <ul>
         {#each files as file}
