6aefbcf588d93d70f894f89304efb25090d6f12f
mkpoli <git@mkpo.li>
2025-01-07 07:15:32 +0900

    feat: add basic file list

---
+++ src/lib/components/FileList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const files: { name: string }[] = [];
+</script>
+
+<section class="bg-gray-200 dark:bg-gray-800">
+  <h2 class="bg-gray-300 dark:bg-black p-2">Audios</h2>
+
+  <div class="p-2 h-full">
+    {#if files.length === 0}
+      <p>No files found</p>
+    {:else}
+      <ul>
+        {#each files as file}
+          <li>{file.name}</li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { invoke } from '@tauri-apps/api/core';
-  import Header from '../lib/components/Header.svelte';
-
+  import Header from '$lib/components/Header.svelte';
+  import FileList from '$lib/components/FileList.svelte';
   let name = $state('');
   let greetMsg = $state('');
 
@@ -15,26 +15,29 @@
 <Header />
 
 <main class="container">
-  <h1 class="text-4xl font-orbitron">Welcome to Tauri + Svelte</h1>
+  <FileList />
+  <div>
+    <h1 class="text-4xl font-orbitron">Welcome to Tauri + Svelte</h1>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://kit.svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
-    </a>
+    <div class="row">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+      </a>
+      <a href="https://tauri.app" target="_blank">
+        <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+      </a>
+      <a href="https://kit.svelte.dev" target="_blank">
+        <img src="/svelte.svg" class="logo svelte-kit" alt="SvelteKit Logo" />
+      </a>
+    </div>
+    <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
+
+    <form class="row" onsubmit={greet}>
+      <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+      <button type="submit">Greet</button>
+    </form>
+    <p>{greetMsg}</p>
   </div>
-  <p>Click on the Tauri, Vite, and SvelteKit logos to learn more.</p>
-
-  <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
-    <button type="submit">Greet</button>
-  </form>
-  <p>{greetMsg}</p>
 </main>
 
 <style>
@@ -66,7 +69,6 @@
     margin: 0;
     padding-top: 10vh;
     display: flex;
-    flex-direction: column;
     justify-content: center;
     text-align: center;
   }
