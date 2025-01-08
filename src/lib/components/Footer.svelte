923babe2a5f5a85200d2365942fb4da3a2f28458
mkpoli <git@mkpo.li>
2025-01-08 11:19:46 +0900

    feat: add basic footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from '$app/state';
+  let currentMode = $derived(
+    page.url.pathname == '/analyze' ? 'Analyse' : page.url.pathname == '/plot' ? 'Plot' : 'Audio'
+  );
+</script>
+
+<footer class="w-full p-4">
+  {currentMode}
+  <nav class="flex items-center justify-center mx-auto font-orbitron text-xl gap-4">
+    <a href="/">Audio</a>
+    <a href="/analyze">Analyse</a>
+    <a href="/plot">Plot</a>
+  </nav>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import Footer from '$lib/components/Footer.svelte';
   import '../app.css';
   import '@fontsource-variable/orbitron';
   let { children } = $props();
 </script>
 
 {@render children()}
+
+<Footer />
